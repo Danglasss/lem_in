2c5419e5cdf3a7cd567583f77d3a6ecd4824b8c8
Danglass M'BOULE <damboule@e1r2p9.42.fr>
2020-01-23 12:16:02 +0100

    a

--- srcs/t_hash.c
+++ srcs/t_hash.c
@@ -6,7 +6,7 @@
 /*   By: damboule <damboule@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 13:52:35 by nabboufe          #+#    #+#             */
-/*   Updated: 2019/09/18 17:11:57 by damboule         ###   ########.fr       */
+/*   Updated: 2020/01/23 12:05:13 by damboule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,6 +102,25 @@ unsigned long	collision(char *to_hash, t_salle *room,
 	return (n_collision(to_hash, room, hashed_s));
 }
 
+
+unsigned long	col(char *to_hash, t_salle *room,
+		unsigned long hashed_s)
+{
+	if (room[hashed_s + 1].salle == NULL
+			|| !ft_strcmp(room[hashed_s + 1].salle, to_hash))
+		return (hashed_s + 1);
+	else if (room[hashed_s + 2].salle == NULL
+			|| !ft_strcmp(room[hashed_s + 2].salle, to_hash))
+		return (hashed_s + 2);
+	else if (room[hashed_s + 3].salle == NULL
+			|| !ft_strcmp(room[hashed_s + 3].salle, to_hash))
+		return (hashed_s + 3);
+	else if (room[hashed_s + 4].salle == NULL
+			|| !ft_strcmp(room[hashed_s + 4].salle, to_hash))
+		return (hashed_s + 4);
+	return (n_collision(to_hash, room, hashed_s));
+}
+
 unsigned long	t_hash(char *to_hash, t_salle *room)
 {
 	unsigned long	hashed_s;
@@ -122,8 +141,10 @@ unsigned long	t_hash(char *to_hash, t_salle *room)
 	hashed_s ^= hashed_s >> 11;
 	hashed_s += hashed_s << 15;
 	hashed_s %= t_size;
-	if (room[hashed_s].salle != NULL &&
-			ft_strcmp(room[hashed_s].salle, to_hash))
+	if (hashed_s == 0)
+		return (col(to_hash, room, hashed_s));
+	if ((room[hashed_s].salle != NULL &&
+			ft_strcmp(room[hashed_s].salle, to_hash)))
 		return (collision(to_hash, room, hashed_s));
 	return (hashed_s);
 }
